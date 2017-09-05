ad4b8b25839a704fca6ee2b49965bff3bed0dc3e
hassan <hah.alex@yahoo.com>
2017-09-05 14:36:48 +0200

    implement blake2s in the extension

--- php_blake2.c
+++ php_blake2.c
@@ -13,6 +13,7 @@
 
 zend_function_entry blake2_functions[] = {
     PHP_FE(blake2, NULL)
+    PHP_FE(blake2s, NULL)
     {NULL, NULL, NULL}
 };
 
@@ -81,6 +82,72 @@ PHP_FUNCTION(blake2)
 
     if (result != 0) {
         zend_error(E_WARNING, "Error generating BLAKE2 hash");
+        RETURN_FALSE;
+    }
+
+    if (rawOutput) {
+#if ZEND_MODULE_API_NO >= 20151012
+        RETURN_STRINGL(hashOutput, hashByteLength);
+#else
+        RETURN_STRINGL(hashOutput, hashByteLength, 1);
+#endif
+    } else {
+        char hex[hashByteLength * 2 + 1];
+        php_hash_bin2hex(hex, (unsigned char *) hashOutput, hashByteLength);
+        hex[hashByteLength * 2] = '\0';
+
+#if ZEND_MODULE_API_NO >= 20151012
+        RETURN_STRING(hex);
+#else
+        RETURN_STRING(hex, 1);
+#endif
+    }
+}
+
+PHP_FUNCTION(blake2s)
+{
+#if ZEND_MODULE_API_NO >= 20151012
+    zend_long hashByteLength = BLAKE2S_OUTBYTES;
+    size_t dataByteLength;
+    size_t keyLength = 0;
+#else
+    long hashByteLength = BLAKE2S_OUTBYTES;
+    int dataByteLength;
+    int keyLength = 0;
+#endif
+    unsigned char *data;
+    unsigned char *key;
+    zend_bool rawOutput = 0;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s|lsb", &data, &dataByteLength, &hashByteLength, &key, &keyLength, &rawOutput) == FAILURE) {
+        return;
+    }
+
+    zend_bool hasError = 0;
+
+    if (hashByteLength < 1) {
+        hasError = 1;
+        zend_error(E_WARNING, "BLAKE2s output length is too short");
+    } else if (hashByteLength > BLAKE2S_OUTBYTES) {
+        hasError = 1;
+        zend_error(E_WARNING, "BLAKE2s output length is too long");
+    }
+
+    if (keyLength > BLAKE2S_KEYBYTES) {
+        hasError = 1;
+        zend_error(E_WARNING, "BLAKE2s key length is too long");
+    }
+
+    if (hasError) {
+        RETURN_FALSE;
+    }
+
+    char hashOutput[hashByteLength];
+
+    int result = blake2s(hashOutput, hashByteLength, data, dataByteLength, key, keyLength);
+
+    if (result != 0) {
+        zend_error(E_WARNING, "Error generating BLAKE2s hash");
         RETURN_FALSE;
     }
 
